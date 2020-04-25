f0d1ca98ad52db88512831334b8bfed0e12bcd75
Code Stack <codestacktech@gmail.com>
2020-04-25 17:18:16 +0530

    init 2

--- app/src/main/java/com/example/roomdatabaseandroid/database/AppRepository.java
+++ app/src/main/java/com/example/roomdatabaseandroid/database/AppRepository.java
@@ -10,7 +10,9 @@ public interface AppRepository {
 
     // user CRUD
     void insertUser(UserEntity user);
+
     List<UserEntity> getAllUsers() throws ExecutionException, InterruptedException;
+
     void deleteUser(UserEntity user);
 
 }
